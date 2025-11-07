08cca9e26a339dce11b5fdd4fcff94623b803f6a
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-07 14:59:17 +0900

    refactor(ready): 봇 실행 시 로그인 성공 메세지 출력 로직 분리

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+import fs from "fs"; //events 폴더나 commands 폴더를 fs로 읽어와야 봇 실행 시 리스너 등록 가능
+import path from "path"; //폴더나 파일 경로가 있어야 가져오기 가능
+import { pathToFileURL } from "url";
 import "dotenv/config";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import pool from "./db/database.js";
 
 const token = process.env.DISCORD_TOKEN;
 
-// Create a new client instance
+// 새로운 클라이언트 인스턴스 생성 = 봇
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -13,9 +16,8 @@ const client = new Client({
   ],
 });
 
+//봇 실행시 처음 한 번만 실행하는 코드
 client.once(Events.ClientReady, async (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-
   try {
     const result = await pool.query("SELECT current_database()");
     console.log("DB 연결 성공", result.rows[0].current_database);
@@ -24,5 +26,34 @@ client.once(Events.ClientReady, async (readyClient) => {
   }
 });
 
-// Log in to Discord with your client's token
+//__dirname은 CJS에서는 지원했지만 esm에서는 하지 않기 때문에 별도로 선언해줘야 함
+const __dirname = import.meta.dirname;
+//events 파일경로
+const eventsPath = path.join(__dirname, "events");
+//events 파일 내부에 .js로 끝나는 파일 전부 읽어서 저장
+const eventFilesDir = await fs.promises.readdir(eventsPath);
+const eventFiles = eventFilesDir.filter((file) => file.endsWith(".js"));
+
+/*
+모든 이벤트 파일에 대해 해당 파일의 이벤트 리스너를 봇(client)에 등록하는 과정
+import에 await가 필요한 이유는 cjs의 require는 동기식으로 작동하지만 esm의 import는 비동기 식이라 가져오는 것을 기다려줘야 함
+
+require와 달리 fileURL을 import 해서 가져오는 건 객체 전체가 담기기 때문에 내부에 여러 속성들이 존재함. 
+하지만 우리는 export default를 한 함수만 필요함 -> 따라서 eventModul.default를 통해 event로 저장
+*/
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const fileURL = pathToFileURL(filePath);
+
+  const eventModule = await import(fileURL);
+  const event = eventModule.default;
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
+// 클라이언트 토큰을 갖고 봇 로그인
 client.login(token);
