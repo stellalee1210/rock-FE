ee5f11b865bf2f37e42cead39a88490974fe1ad5
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-11 19:42:24 +0900

    refactor(voiceChannelState): get을 활용하여 음성채널을 효율적으로 찾을 수 있게 로직 수정

--- events/voiceChannelState.js
+++ events/voiceChannelState.js
@@ -34,13 +34,8 @@ const setUserInfo = (oldState, newState, client) => {
 const sendMessage = (msg) => {
   const channelCollection = userInfo.curState.guild.channels.cache; //서버에 존재하는 모든 채널을 Collection 형태로 리턴
   //Collection을 순회하여 동일한 voice Channel id를 가진 객체를 찾은 뒤, 해당 객체.send('메세지') 해야 함
-  for (const channel of channelCollection) {
-    const channelId = channel[0];
-    if (channelId === studyChannelId) {
-      const voiceChannelObject = channel[1];
-      voiceChannelObject.send(msg);
-    }
-  }
+  const voiceChannel = channelCollection.get(studyChannelId);
+  voiceChannel.send(msg);
 };
 
 const checkStudy = (client) => {
