283594069380a01ced264b9d73e4e29d3fbbb281
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-23 11:26:22 +0900

    refactor(DashboardContent): 공부시간 가져오는 로직 위치 이동 및 파일명 변경

--- dashboard/src/components/DashboardContent.jsx
+++ dashboard/src/components/DashboardContent.jsx
@@ -1,35 +1,14 @@
-import { useEffect, useRef } from 'react';
 import AttendanceCalendar from './AttendanceCalendar.jsx';
 import { AttendanceCard } from './AttendanceCard.jsx';
-import { RankingCard } from './RankingCard.jsx';
-import { getWeeklyStudyTime } from '../database/studyTime.js';
+import { StudyTimeCard } from './StudyTimeCard.jsx';
 
-export default function DashboardContent({ userId, selectedGuild}){
-    // mock 데이터
-    const rankingData = [
-        { username: '여빈', hours: 45 },
-        { username: '희주', hours: 38 },
-        { username: '돌쇠', hours: 32 },
-        { username: '이슬', hours: 30 },
-    ];
-
-    const hasFetchedData = useRef(false);
-    useEffect(()=>{
-        hasFetchedData.current = false
-        const weeklyStudyTime = async() => {
-            const studyTimeData = await getWeeklyStudyTime(hasFetchedData, userId, selectedGuild);
-            if(studyTimeData) console.log('weekly studyTimeData : ', studyTimeData);
-        }
-    
-    weeklyStudyTime();
-    }, [selectedGuild])
-    
+export default function DashboardContent({ userId, selectedGuild}){ 
     return (
         <>
             <AttendanceCalendar guildId={selectedGuild.id} userId={userId} />
             <div className="flex justify-center gap-12 flex-wrap text-black mt-6">
                 <AttendanceCard userId = { userId } selectedGuild = { selectedGuild } />
-                <RankingCard ranking={rankingData} />
+                <StudyTimeCard userId = { userId } selectedGuild = { selectedGuild } />
             </div>
         </>
     );
